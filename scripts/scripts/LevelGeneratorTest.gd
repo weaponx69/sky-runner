7a8f8949c4152cf66985b539d6b47048c629fadb
weaponx69 <perry.brian@gmail.com>
2025-09-03 13:34:01 -0400

    Trying to get the tests to work.

---
+++ scripts/scripts/LevelGeneratorTest.gd
@@ -0,0 +1,13 @@
+# GdUnit generated TestSuite
+class_name LevelGeneratorTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source: String = 'res://scripts/LevelGenerator.gd'
+
+
+func test__ready() -> void:
+    # remove this line and complete your test
+    assert_not_yet_implemented()
